d7aee4ea67dd96c2d389994e3f58950b6ef0cee0
madeinchema <chemavg1@gmail.com>
2020-11-11 23:37:28 +0100

    Completed Exercise 01 + Extra Credit (Test: PASS)

--- react-hooks-main/src/exercise/01.js
+++ react-hooks-main/src/exercise/01.js
@@ -3,7 +3,7 @@
 
 import * as React from 'react'
 
-function Greeting({initialName}) {
+function Greeting({initialName = ''}) {
   const [name, setName] = React.useState(initialName);
 
   function handleChange(event) {
@@ -22,7 +22,7 @@ function Greeting({initialName}) {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName='Chema' />
 }
 
 export default App
