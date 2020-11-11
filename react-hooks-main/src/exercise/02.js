33ac2b1b0cd1b2f31071172d1890a115e1c59306
madeinchema <chemavg1@gmail.com>
2020-11-11 23:51:15 +0100

    Completed Exercise 02 and 02.1

--- react-hooks-main/src/exercise/02.js
+++ react-hooks-main/src/exercise/02.js
@@ -4,13 +4,17 @@
 import * as React from 'react'
 
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+  const [name, setName] = React.useState(() =>
+    window.localStorage.getItem('name') || initialName
+  );
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name);
+  });
 
   function handleChange(event) {
     setName(event.target.value)
@@ -19,7 +23,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -27,7 +31,7 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName='Chema' />
 }
 
 export default App
